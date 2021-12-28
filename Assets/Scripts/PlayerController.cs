87c833968125f5c404d9900cb7100fe4f293d773
Mirna Pibernik <mpibernik@croz.net>
2021-12-28 21:59:11 +0100

    Add score and lives (trigger, GUI), level restart and load next, 1 enemy movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
@@ -15,35 +17,52 @@ public class PlayerController : MonoBehaviour
     bool facingRight = true;
     float moveDirection = 0;
     bool isGrounded = false;
-    Rigidbody2D r2d;
+    Rigidbody2D rb;
     CapsuleCollider2D mainCollider;
     Transform t;
     private float moveValue = 0f;
+
+    // UI
+    public Text scoreText;
+    static int score;
+    public Text livesText;
+    private int lives = 1;
+    private GameObject gameOver;
+    private GameObject restartButton;
 
     // Use this for initialization
     void Start()
     {
         t = transform;
-        r2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         mainCollider = GetComponent<CapsuleCollider2D>();
-        r2d.freezeRotation = true;
-        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        r2d.gravityScale = gravityScale;
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.gravityScale = gravityScale;
         facingRight = t.localScale.x > 0;
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText.text = score.ToString();
+        livesText = GameObject.Find("LivesText").GetComponent<Text>();
+        livesText.text = lives.ToString();
+        gameOver = GameObject.Find("GameOver");
+        restartButton = GameObject.Find("RestartButton");
+        gameOver.SetActive(false);
+        restartButton.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Movement controls
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && (isGrounded || Mathf.Abs(r2d.velocity.x) > 0.01f))
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) && (isGrounded || Mathf.Abs(rb.velocity.x) > 0.01f))
         {
             moveDirection = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) ? -1 : 1;
             moveValue = 1f;
         }
         else
         {
-            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            if (isGrounded || rb.velocity.magnitude < 0.01f)
             {
                 moveDirection = 0;
                 moveValue = 0f;
@@ -68,8 +87,19 @@ public class PlayerController : MonoBehaviour
         // Jumping
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
         }
+
+        // End game conditions
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+        if (lives == 0 || !GeometryUtility.TestPlanesAABB(planes, mainCollider.bounds))
+        {
+            score = 0;
+            Time.timeScale = 0;
+            gameOver.SetActive(true);
+            restartButton.SetActive(true);
+        }
+
     }
 
     void FixedUpdate()
@@ -94,10 +124,36 @@ public class PlayerController : MonoBehaviour
         }
 
         // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+        rb.velocity = new Vector2((moveDirection) * maxSpeed, rb.velocity.y);
 
         // Simple debug
         Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
         Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            lives--;
+            livesText.text = lives.ToString();
+        }
+        if (collision.gameObject.CompareTag("Fruit"))
+        {
+            lives++;
+            livesText.text = lives.ToString();
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            if (SceneManager.GetActiveScene().name == "FirstLevel") SceneManager.LoadScene("SecondLevel");
+            if (SceneManager.GetActiveScene().name == "SecondLevel") SceneManager.LoadScene("ThirdLevel");
+        }
     }
 }
